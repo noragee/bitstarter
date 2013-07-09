732c6978bbf678fb9041e5f3e285682f21613bd9
Nora Gee <noragee@sbcglobal.net>
2013-07-09 09:37:34 +0000

    Fixed typo in web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 var fs = require("fs");
 var buffer = fs.readFileSync("index.html");
-var str = buffer.ToString();
+var str = buffer.toString();
 app.get('/', function(request, response) {
   response.send(str);
 });
