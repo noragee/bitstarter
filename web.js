7c69181a316bd9ba07a7ee9a4cc43628bb602923
Ubuntu <ubuntu@ip-172-31-18-86.us-west-2.compute.internal>
2013-07-19 07:02:20 +0000

    Updated web.js and index.html for homework 4 part 1

--- web.js
+++ web.js
@@ -3,13 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require("fs");
-var buffer = fs.readFileSync("index.html");
-var str = buffer.toString();
 app.get('/', function(request, response) {
+  var buffer = fs.readFileSync("index.html");
+  var str = buffer.toString();
   response.send(str);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
